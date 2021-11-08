1fff72392a6c24dec32685ffd6dd5600a50fd299
Luppass <93378366+Luppass@users.noreply.github.com>
2021-11-08 09:37:35 +0100

    Delete p2 directory

--- p2/delete.c
+++
@@ -1,5 +0,0 @@
-#include "listar.h"
-int main (int argc, char *argv[]){
-    borrar(argv, argc, cmd_delete(argv, argc));
-    return 0;
-}
--- p2/list.c
+++
@@ -1,6 +0,0 @@
-#include "listar.h"
-
-int main (int argc, char *argv[]){
-    shell_listar(argv, argc);
-    return 0;
-}
